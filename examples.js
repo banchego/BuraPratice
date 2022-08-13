99fb8cf8487f1ddde4ea9a9472b655094ab6f0c7
banchego <nbnikitanb@gmail.com>
2022-08-12 23:26:17 -0600

    Destructuring

--- examples.js
+++ examples.js
@@ -1,3 +1,4 @@
+'use strict';
 //Rest parameter Один и последний
 
 max(1,3);
@@ -8,3 +9,24 @@ function max(a, b, ...numbers){
 console.log(numbers);
 
 }
+/* Destructuring - деструктуризация - позволяет достать значение,
+ с какой либо структуры данных. */
+
+ const person = {
+    name: {
+        first: 'Nick',
+        last : 'Bitkov'
+    },
+    // firstName: 'Peter',
+    // lastName: 'Smith',
+    age: 27
+ };
+ // const firstName = person.firstName;
+ 
+//  const {firstName, lastName} = person;
+
+//  const { name: {first: firstName, last: lastName}} = person; // saved in const firstName ( name: firstName)
+//  console.log(firstName, lastName);
+ 
+ const { permissions:{role : 'user'} = {} } = person; 
+console.log(role);
